d07ec06aa33cc1cf2ce6138828b9649b98479e51
Lamhotjm <lamhotjms@gmail.com>
2020-02-21 10:12:45 -0600

    CommonDivisor

---
+++ src/leetcode_easy/CommonDivisor.java
@@ -0,0 +1,13 @@
+package leetcode_easy;
+
+public class CommonDevisor {
+
+    public static void main(String args[])
+    {
+
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+     return null;
+    }
+}
