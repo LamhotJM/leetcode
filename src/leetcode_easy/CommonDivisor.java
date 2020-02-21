51a46f97725fedb22617e2184e9399d3b5a1f1cb
Lamhotjm <lamhotjms@gmail.com>
2020-02-21 11:36:59 -0600

    nim game

--- src/leetcode_easy/CommonDivisor.java
+++ src/leetcode_easy/CommonDivisor.java
@@ -1,13 +1,26 @@
 package leetcode_easy;
+import java.util.regex.Pattern;
+public class CommonDivisor {
 
-public class CommonDevisor {
 
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
 
+        CommonDivisor cd = new CommonDivisor();
+        System.out.println(cd.gcdOfStrings("ABABABAB","ABAB"));
     }
 
     public String gcdOfStrings(String str1, String str2) {
-     return null;
+        int count =0;
+        if (str1.length() > str2.length())
+            return gcdOfStrings(str2, str1);
+        else if (!str2.startsWith(str1))
+            return "";
+        else if (str1.isEmpty())
+            return str2;
+        else
+            count ++;
+            System.out.println(count);
+            return gcdOfStrings(str1, str2.substring(str1.length()));
+
     }
 }
